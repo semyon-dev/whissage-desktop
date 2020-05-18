f71421d5ff672bfa9dccc2b284087fbd7bec63ea
semyon-dev <semyon-dev@protonmail.com>
2020-05-19 01:16:55 +0300

    feat: send()

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"github.com/semyon-dev/whissage-desktop/model"
+	"strconv"
+	"time"
 )
 
 var a fyne.App
+var username string
 
 func main() {
 	a = app.New()
 	w := a.NewWindow("Whissage")
 	w.CenterOnScreen()
 	w.Resize(fyne.NewSize(400, 400))
+
+	usernameEntry := widget.NewEntry()
+
 	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Login:"),
-		widget.NewEntry(),
+		widget.NewLabel("Your username:"),
+		usernameEntry,
 		widget.NewButton("Let's go", func() {
-			w.Close()
-			chatWindow()
+			if usernameEntry.Text != "" {
+				username = usernameEntry.Text
+				w.Close()
+				chatWindow()
+			}
 		}),
 	))
 
@@ -26,19 +38,33 @@ func main() {
 }
 
 func chatWindow() {
-	w := a.NewWindow("Chat")
+	w := a.NewWindow("Chat with " + username)
 	w.CenterOnScreen()
 	w.Resize(fyne.NewSize(400, 400))
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("New message"),
-		widget.NewEntry(),
-		widget.NewButton("Send", func() {
-			send()
-		}),
-	))
+
+	messageEntry := widget.NewEntry()
+
+	content := widget.NewVBox(widget.NewLabel(""),
+		widget.NewHBox(
+			widget.NewLabel("New message"),
+			messageEntry,
+			widget.NewButton("Send", func() {
+				send(messageEntry.Text)
+			})))
+
+	w.SetContent(content)
 	w.Show()
+
+	content.Append(widget.NewButton("Add more items", func() {
+		content.Append(widget.NewLabel("message"))
+	}))
 }
 
-func send() {
-
+func send(text string) {
+	var msg model.Message
+	msg.Message = text
+	msg.User = username
+	msg.Time = strconv.Itoa(int(time.Now().Unix()))
+	msgBytes, _ := json.Marshal(msg)
+	fmt.Println(msgBytes)
 }
