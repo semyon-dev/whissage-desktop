cb3a6347fe417da0421dcc447c07bbaff6722520
semyon-dev <semyon-dev@protonmail.com>
2020-05-19 01:44:33 +0300

    feat: conn close and json parse

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/semyon-dev/whissage-desktop
 
 go 1.14
 
-require fyne.io/fyne v1.2.4
+require (
+	fyne.io/fyne v1.2.4
+	github.com/gorilla/websocket v1.4.2
+)
--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"github.com/gorilla/websocket"
+	"github.com/semyon-dev/whissage-desktop/config"
 	"github.com/semyon-dev/whissage-desktop/model"
+	"log"
+	"net/url"
 	"strconv"
 	"time"
 )
@@ -37,6 +41,8 @@ func main() {
 	w.ShowAndRun()
 }
 
+var content *widget.Box
+
 func chatWindow() {
 	w := a.NewWindow("Chat with " + username)
 	w.CenterOnScreen()
@@ -44,20 +50,33 @@ func chatWindow() {
 
 	messageEntry := widget.NewEntry()
 
-	content := widget.NewVBox(widget.NewLabel(""),
+	content = widget.NewVBox(widget.NewLabel(""),
 		widget.NewHBox(
 			widget.NewLabel("New message"),
 			messageEntry,
 			widget.NewButton("Send", func() {
 				send(messageEntry.Text)
-			})))
+			})),
+		widget.NewButton("Quit & close connection", func() {
+			closeConn()
+			w.Close()
+			a.Quit()
+		}))
 
 	w.SetContent(content)
 	w.Show()
 
-	content.Append(widget.NewButton("Add more items", func() {
-		content.Append(widget.NewLabel("message"))
-	}))
+	connect()
+}
+
+func appendMessage(text []byte) {
+	var data model.Message
+	err := json.Unmarshal(text, &data)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		content.Append(widget.NewLabel("from: " + data.User + " - " + data.Message))
+	}
 }
 
 func send(text string) {
@@ -66,5 +85,50 @@ func send(text string) {
 	msg.User = username
 	msg.Time = strconv.Itoa(int(time.Now().Unix()))
 	msgBytes, _ := json.Marshal(msg)
-	fmt.Println(msgBytes)
+	err := c.WriteMessage(websocket.TextMessage, msgBytes)
+	if err != nil {
+		fmt.Println("write:", err)
+	}
+}
+
+var c *websocket.Conn
+
+func closeConn() {
+	// Cleanly close the connection by sending a close message and then
+	// waiting (with timeout) for the server to close the connection.
+	defer c.Close()
+	err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		fmt.Println("write close:", err)
+	}
+}
+
+func connect() {
+
+	//interrupt := make(chan os.Signal, 1)
+	//signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: config.Url, Path: "/ws/"}
+	fmt.Printf("connecting to %s", u.String())
+
+	var err error
+	c, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				fmt.Println("read:", err)
+				return
+			}
+			fmt.Printf("recv: %s", message)
+			appendMessage(message)
+		}
+	}()
 }
